e6ae0a1552b5c124b50820b04fd8def8f572baec
Usuario <Usuario@CPD03-M05>
2024-05-13 12:20:48 -0300

    Adicionar arquivos de projeto.

---
+++ buscaCep/Models/Cidade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buscaCep.Models
+{
+    internal class Cidade
+    {
+        public int id_cidade { get; set; }
+        public string descricao { get; set; }
+        public string uf { get; set; }
+        public int codigo_ibge { get; set; }    
+        public int ddd {  get; set; }
+
+    }
+}
---
+++ buscaCep/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace buscaCep
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
